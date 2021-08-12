22b912aaf0332d2a1d36d7a7865490a2769b32f1
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-08-12 10:48:09 -0400

    className error
    fixed issue with class= being called in main nav instead of className=

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
-const Nav = props =>( <nav class="main-nav">
+const Nav = props =>( <nav className="main-nav">
         <ul>
             <li><NavLink to="/cats">Cats</NavLink></li>
             <li><NavLink to="/dogs">Dogs</NavLink></li>
